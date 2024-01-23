19a033ae98bf0857ea6ebb9b0d620f00a64139a2
ShayanKhan333 <shayan.13579.khan@gmail.com>
2024-01-24 03:22:58 +0500

    Initial commit

---
+++ practice 1/index.js
@@ -0,0 +1,73 @@
+// For loops
+
+// let a=1
+
+// for (let i = 0; i <=100; i++) {
+//   console.log(i)
+// }
+// let arr=["shayan","ayan","hafsa","nasreen","maqsood"]
+// for (let i = 0; i < arr.length; i++){
+//     console.log(i,arr)
+// }
+
+//For in loops
+
+// let obj={
+//    name:"shayan",
+//    role:"busniessman",
+//     company:"flexiKick"
+// }
+
+// for (const key in obj) {
+//         const element = obj[key];
+//         console.log(key,element)
+// }
+
+//For of loop
+
+
+// array type loop
+// for (const c of "shayan") {
+// console.log(c)
+
+//     }
+//while loop
+// let i=2
+// while (i<7) {
+//     console.log(i);
+//     i++
+// }
+//do while loop
+// let i=0
+// do {
+//     console.log(i)
+//     i++
+// } while (i<=5);
+
+// practice
+// Qno1
+// let userAge =prompt("enter your age")
+// userAge=parseInt(userAge)
+// for (let i = 1; i <=1; i++) {
+//     userAge=   userAge + 1;
+//     console.log("next year your age is "+userAge)
+// }
+
+
+// let user=prompt("enter no")
+
+// user=parseInt(user)
+// user =user +2
+
+// console.log(user)
+
+
+
+
+
+
+
+
+
+
+
